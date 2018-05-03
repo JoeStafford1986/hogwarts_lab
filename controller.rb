729654356521b70aed79cd34322f8951ece8f808
Joe Stafford <joestafford1986@gmail.com>
2018-05-03 14:16:18 +0100

    show student page and add new student

--- controller.rb
+++ controller.rb
@@ -9,7 +9,17 @@ get '/' do
   erb( :index )
 end
 
-get '/show_students' do
+get '/students' do
   @students = Student.all()
-  erb( :show_students )
+  erb( :students )
+end
+
+get '/students/new' do
+  erb(:new)
+end
+
+post '/students' do
+  @student = Student.new(params)
+  @student.save()
+  redirect '/students'
 end

512d6240bbd4a8b0e26b2c90ca3effb703a164ac
Joe Stafford <joestafford1986@gmail.com>
2018-05-03 12:11:22 +0100

    methods added

--- models/house.rb
+++ models/house.rb
@@ -23,4 +23,45 @@ class House
     @id = house_data.first()['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE students
+    SET
+    (
+      name
+    )
+    VALUES
+    (
+      $1
+    )
+    WHERE id = $2"
+    values = [@name]
+    SqlRunner.run( sql, values )
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM houses;"
+    SqlRunner.run(sql)
+  end
+
+  def delete()
+    sql = "DELETE FROM houses
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run( sql, values )
+  end
+
+  def self.all()
+    sql = "SELECT * FROM houses"
+    houses = SqlRunner.run( sql )
+    result = houses.map { |house| house.new( house ) }
+    return result
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM houses WHERE id = $1"
+    values = [id]
+    house = SqlRunner.run( sql, values )
+    result = House.new( house.first )
+    return result
+  end
 end
--- models/student.rb
+++ models/student.rb
@@ -36,4 +36,52 @@ attr_reader :id, :first_name, :last_name, :age, :house, :house_id
     @id = student_data.first()['id'].to_i
   end
 
+  def update()
+    sql = "UPDATE students
+    SET
+    (
+      first_name,
+      last_name,
+      age,
+      house,
+      house_id
+    )
+    VALUES
+    (
+      $1, $2, $3, $4, $5
+    )
+    WHERE id = $6"
+    values = [@first_name, @last_name, @age, @house, @house_id]
+    SqlRunner.run( sql, values )
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM students;"
+    SqlRunner.run(sql)
+  end
+
+  def delete()
+    sql = "DELETE FROM students
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run( sql, values )
+  end
+
+  def self.all()
+    sql = "SELECT * FROM students"
+    students = SqlRunner.run( sql )
+    result = students.map { |student| Student.new( student ) }
+    return result
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM students WHERE id = $1"
+    values = [id]
+    student = SqlRunner.run( sql, values )
+    result = Student.new( student.first )
+    return result
+  end
+
+
+
 end
